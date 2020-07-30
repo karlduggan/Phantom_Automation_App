90f3d130021cd8a3819379b01bbf1fedc0359eed
Karl Duggan <52557899+karlduggan@users.noreply.github.com>
2020-07-30 12:59:47 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,221 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from phantomClass import Phantom
+from threading import Thread
+import pickle
+
+
+class Ui_Form(object):
+    def __init__(self):
+        # Phantom Automation class
+        self.phantom = Phantom()
+        self.trackSwitch = False
+        
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(530, 393)
+        
+        self.verticalLayout = QtWidgets.QVBoxLayout(Form)
+        self.verticalLayout.setObjectName("verticalLayout")
+        
+        
+        self.widget = QtWidgets.QWidget(Form)
+        self.widget.setObjectName("widget")
+        
+        self.gridLayout = QtWidgets.QGridLayout(self.widget)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setObjectName("gridLayout")
+        
+        self.pushButton_add = QtWidgets.QPushButton(self.widget)
+        self.pushButton_add.setObjectName("pushButton_add")
+        self.gridLayout.addWidget(self.pushButton_add, 7, 2, 1, 1)
+        
+        self.pushButton_delete = QtWidgets.QPushButton(self.widget)
+        self.pushButton_delete.setObjectName("pushButton_delete")
+        self.gridLayout.addWidget(self.pushButton_delete, 6, 2, 1, 1)
+        self.pushButton_delete.clicked.connect(self.deleteAtion)
+        
+        self.lineEdit_typeText = QtWidgets.QLineEdit(self.widget)
+        self.lineEdit_typeText.setText("")
+        self.lineEdit_typeText.setObjectName("lineEdit_typeText")
+        self.gridLayout.addWidget(self.lineEdit_typeText, 9, 1, 1, 1)
+        
+        self.label_text = QtWidgets.QLabel(self.widget)
+        self.label_text.setObjectName("label_text")
+        self.gridLayout.addWidget(self.label_text, 9, 0, 1, 1)
+        
+        self.pushButton_capture = QtWidgets.QPushButton(self.widget)
+        self.pushButton_capture.setObjectName("pushButton_capture")
+        self.gridLayout.addWidget(self.pushButton_capture, 0, 2, 1, 1)
+        self.pushButton_capture.clicked.connect(self.captureAction)
+        
+        self.checkBox_doubleClick = QtWidgets.QCheckBox(self.widget)
+        self.checkBox_doubleClick.setObjectName("checkBox_doubleClick")
+        self.gridLayout.addWidget(self.checkBox_doubleClick, 1, 2, 1, 1)
+        
+        self.checkBox_leftClick = QtWidgets.QCheckBox(self.widget)
+        self.checkBox_leftClick.setObjectName("checkBox_leftClick")
+        self.gridLayout.addWidget(self.checkBox_leftClick, 2, 2, 1, 1)
+        
+        self.checkBox_rightClick = QtWidgets.QCheckBox(self.widget)
+        self.checkBox_rightClick.setObjectName("checkBox_rightClick")
+        self.gridLayout.addWidget(self.checkBox_rightClick, 3, 2, 1, 1)
+        
+        self.checkBox_loopClick = QtWidgets.QCheckBox(self.widget)
+        self.checkBox_loopClick.setObjectName("checkBox_loopClick")
+        self.gridLayout.addWidget(self.checkBox_loopClick, 4, 2, 1, 1)
+        
+        self.widget_saveload = QtWidgets.QWidget(self.widget)
+        self.widget_saveload.setObjectName("widget_saveload")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.widget_saveload)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        
+        self.pushButton_save = QtWidgets.QPushButton(self.widget_saveload)
+        self.pushButton_save.setObjectName("pushButton_save")
+        self.horizontalLayout.addWidget(self.pushButton_save)
+        self.pushButton_save.clicked.connect(self.saveFunc)
+        
+        self.pushButton_load = QtWidgets.QPushButton(self.widget_saveload)
+        self.pushButton_load.setObjectName("pushButton_load")
+        self.horizontalLayout.addWidget(self.pushButton_load)
+        self.pushButton_load.clicked.connect(self.loadFunc)
+        
+        self.gridLayout.addWidget(self.widget_saveload, 9, 2, 1, 1)
+        self.listWidget = QtWidgets.QListWidget(self.widget)
+        self.listWidget.setObjectName("listWidget")
+        self.gridLayout.addWidget(self.listWidget, 0, 0, 9, 2)
+        spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.gridLayout.addItem(spacerItem, 4, 2, 1, 1)
+    #
+        self.pushButton_clear = QtWidgets.QPushButton(self.widget)
+        self.pushButton_clear.setObjectName("pushButton_clear")
+        self.gridLayout.addWidget(self.pushButton_clear, 8, 2, 1, 1)
+        self.pushButton_clear.clicked.connect(self.clearAction)
+        
+        self.pushButton_play = QtWidgets.QPushButton(self.widget)
+        self.pushButton_play.setObjectName("pushButton_play")
+        self.gridLayout.addWidget(self.pushButton_play, 5, 2, 1, 1)
+        self.pushButton_play.clicked.connect(self.playAction)
+        
+        self.verticalLayout.addWidget(self.widget)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Phantom List"))
+        self.pushButton_add.setText(_translate("Form", "Add"))
+        self.pushButton_delete.setText(_translate("Form", "Delete"))
+        self.label_text.setText(_translate("Form", "Text :"))
+        self.pushButton_capture.setText(_translate("Form", "Capture / Ctrl + A"))
+        self.pushButton_capture.setShortcut(_translate("Form", "Meta+A"))
+        self.checkBox_doubleClick.setText(_translate("Form", "Double Click / Ctrl + Q"))
+        self.checkBox_doubleClick.setShortcut(_translate("Form", "Meta+Q"))
+        self.checkBox_leftClick.setText(_translate("Form", "Left Click / Ctrl + W"))
+        self.checkBox_leftClick.setShortcut(_translate("Form", "Meta+W"))
+        self.checkBox_rightClick.setText(_translate("Form", "Right Click / Ctrl + E"))
+        self.checkBox_rightClick.setShortcut(_translate("Form", "Meta+E"))
+        self.checkBox_loopClick.setText(_translate("Form", "Loop"))
+        self.pushButton_save.setText(_translate("Form", "Save"))
+        self.pushButton_load.setText(_translate("Form", "Load"))
+        self.pushButton_clear.setText(_translate("Form", "Clear"))
+        self.pushButton_play.setText(_translate("Form", "Play"))
+        
+# Button Functions
+
+    # Update ListWidget with data
+    def update_listWidget(self):
+        # Clear the listWidget
+        
+        self.listWidget.clear()
+        data = self.phantom.get_dataList()
+        for i in data:
+            self.listWidget.addItem(str(i))
+           
+    # Capture and submit data 
+    def captureAction(self):
+        # return true or false with isChecked()
+        
+        double_clk = self.checkBox_doubleClick.isChecked()
+        left_clk = self.checkBox_leftClick.isChecked()
+        right_clk = self.checkBox_rightClick.isChecked()
+        
+        # Check Text type is check, if yes then collect text
+        text = None
+        if self.lineEdit_typeText.text() != '':
+            text = self.lineEdit_typeText.text()
+
+        
+        self.phantom.capture(double_clk,left_clk,right_clk,text)
+        # Update the view with new data
+        self.update_listWidget()
+        # After Submit the checkboxes must be unchecked for the next submission 
+        self.checkBox_doubleClick.setChecked(False)
+        self.checkBox_leftClick.setChecked(False)
+        self.checkBox_rightClick.setChecked(False)
+        self.lineEdit_typeText.clear()
+    
+    # Play Button  play and stop
+    def playAction(self):
+        # Check that data list is not empty
+        if self.phantom.get_dataList() != []:    
+            self._play()
+            
+        else:
+            print('Data List Empty')
+        
+    def _play(self):
+        # Run automation through Threading
+        t = Thread(target=self.phantom.runAuto)
+        t.start()
+        
+    # Save Function 
+    def saveFunc(self):
+        name = QtWidgets.QFileDialog.getSaveFileName(Form,'Save File')
+        filename = name[0]
+        with open(filename + '.pkl', 'wb') as file:
+            data = self.phantom.get_dataList()
+            pickle.dump(data, file)
+
+
+    # Load Function 
+    def loadFunc(self):
+        try: 
+            name = QtWidgets.QFileDialog.getOpenFileName(Form, 'Open File')
+            filename = name[0]
+            with open(filename, 'rb') as file:
+                
+                data = pickle.load(file)
+                self.phantom.set_datalist(data)
+            
+                # Update the list with loaded data
+                self.update_listWidget()    
+        except:
+            return
+    
+    # Clear data function
+    def clearAction(self):
+        self.phantom.clear_datalist()
+        # Update list widget
+        self.update_listWidget()    
+    
+            
+    # Delete selected function
+    def deleteAtion(self):
+        # Get selected item from list widget
+        item = self.listWidget.currentItem().text()
+        # Remove the selected item from data list
+        self.phantom.delete_datalist(item)
+        
+        # Update list
+        self.update_listWidget()
+        
+        
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
---
+++ phantomClass.py
@@ -0,0 +1,157 @@
+import pyautogui as p
+from threading import Thread
+import time 
+
+class Phantom(object):
+    def __init__(self):
+        self.active = False
+        self.data_list = []
+        self.speed = 0.1
+        self.sleep = 3
+ 
+        # The atributes will feeds out to the gui framework
+        self.xpos = 0
+        self.ypos = 0
+    
+    # Used for saving data
+    def get_dataList(self):
+        return self.data_list
+    
+    # Used for loading data
+    def set_datalist(self, data):
+        self.data_list = data
+        
+    def get_activeStatus(self):
+        return self.active
+    
+    # Clear data list
+    def clear_datalist(self):
+        self.data_list = []
+    
+    # Undo previous entry
+    def undo_datalist(self):
+        if self.data_list != []:
+            index = len(self.data_list) -1
+            self.data_list.pop(index)
+        else:
+            return False
+    
+    # Entry number / x position / y position / double left click / left click / right click / text
+    def insert_data(self, xpos, ypos, dclk=False, lclk=False, rclk=False, text=None):
+        entry_num = len(self.data_list)
+        self.data_list.append([entry_num,xpos,ypos,dclk,lclk,rclk,text])
+    
+    def setAuto_speed(self, num):
+        self.speed = num
+        
+    def read_data(self):
+        if self.data_list == []:
+            return None
+        else:
+            for i in range(len(self.data_list)):
+                print(self.data_list[i])
+                
+    # Run automation using pyautogui
+    def runAuto(self):
+        
+        time.sleep(0.3)
+        if len(self.data_list) > 0:
+            self.active = True
+            
+            index = 0
+            while self.active and index < len(self.data_list):
+                
+                # Position automation check
+                xpos = self.data_list[index][1]
+                ypos = self.data_list[index][2]
+                p.moveTo(xpos,ypos, 0.4)
+                
+                # Check Double click Boolean 
+                if self.data_list[index][3] == True:
+                    p.doubleClick()
+                    
+                # Check Left click Boolean
+                if self.data_list[index][4] == True:
+                    p.click(button='left')
+                
+                # Check right click Boolean
+                if self.data_list[index][5] == True:
+                    p.click(button='right')
+                
+                # Check Text is not equal to None
+                if self.data_list[index][6] != None:
+                    p.typewrite(str(self.data_list[index][6]))
+                 
+                index += 1
+         
+    # Implemented threading to process separately for multiprocessing 
+    def runAutoThread(self):
+        x = Thread(target=self.runAuto)
+        x.start()
+        x.join()
+   
+    # Need to iterate through the function to get output due to the yield
+    def __tracking(self):
+            while self.active == True:
+                # Gets the position of the mouse in a tuple 
+                pos = p.position()
+                xpos = pos[0]
+                ypos = pos[1]
+                # Update time every 0.3 second
+                time.sleep(self.speed)
+                yield xpos, ypos 
+
+    # Iterate through the yield object 
+    def _tracking(self):
+        for xpos, ypos in self.__tracking():
+            # Update the position attributes
+            self.update_XY(xpos,ypos)
+            # Print in the terminal for confirmation
+            print(xpos, ypos)
+            
+    # Preparing Threading for multiprocessing, The Threading will be activated on main.py
+    # The function below allows a stop / start action
+    def startTracking(self):
+        if self.active == False:
+            self.active = True
+            self._tracking()
+        else:
+            self.active = False
+        
+    # Gets all required information and the passes to insert_data method
+    def capture(self, dclk, lclk, rclk, text):
+        # Get position
+        pos = p.position()
+        xpos = pos[0]
+        ypos = pos[1]
+        
+        self.insert_data(xpos,ypos,dclk,lclk,rclk,text)
+    
+    def changeActive(self, value):
+        self.active = value
+    
+    def update_XY(self, xpos, ypos):
+        self.xpos = xpos
+        self.ypos = ypos
+    
+    # Delete item from list function
+    def delete_datalist(self, item):
+        # Get the index value from the selection
+        index = int(item[1])
+        # Position within the list
+        position = None
+        # Iterate through the data list to find matching index
+        for i in range(len(self.data_list)):
+            itemx = self.data_list[i]
+            if itemx[0] == index:
+                position = i
+        # Using the pop method to remove the item at postion in list
+        self.data_list.pop(position)
+        
+        # Update the list index from 0 till length of list
+        num = 0
+        for index in self.data_list:
+            index[0] = num
+            num += 1
+
+    
